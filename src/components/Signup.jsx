bd563ee6ff5b5acb471810ca0aef4926e1c07951
heisenberg <sda@example.com>
2023-03-06 19:35:43 +0800

    sda

---
+++ src/components/Signup.jsx
@@ -0,0 +1,19 @@
+import styles from "../css/Signup.module.css"
+
+export const Signup=()=>{
+    return(
+        <div className={styles.container}>
+            <div className={styles.form}>
+                <div className={styles.bigText}>Sign up</div>
+
+                <input/>
+
+                <input/>
+
+                <input/>
+
+                <button>Done</button>
+            </div>
+        </div>
+    )
+}
